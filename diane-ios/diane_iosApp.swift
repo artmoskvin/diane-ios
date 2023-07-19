4071e326b65ed21552b9cc8cdca6c98e4096f4b5
Artem Moskvin <artemm@spotify.com>
2023-07-19 20:22:58 +0200

    Initial Commit

---
+++ diane-ios/diane_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  diane_iosApp.swift
+//  diane-ios
+//
+//  Created by Artem Moskvin on 2023-07-19.
+//
+
+import SwiftUI
+
+@main
+struct diane_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
